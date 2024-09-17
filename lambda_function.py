5ada435c304c8771f1695ed31ae3dc404ca86b6d
Noam Ben Mordechai <noam112113@gmail.com>
2024-09-17 18:12:35 +0300

    added $₪ symbols

--- lambda_function.py
+++ lambda_function.py
@@ -19,11 +19,11 @@ def lambda_handler(event, context):
         if most_recent_open < average_open * 0.85:
             subject = f'VOO Alert: Stock Price Drop - Buy Now!'
             message = (
-                f"ALERT! VOO opened at {most_recent_open}, which is more than 15% below the average open price over the past month.\n\n"
-                f"Average open price: {average_open}\n"
-                f"Current price: {most_recent_open}\n\n"
-                f"the price of USD is currently {currency.usd_price}, and a VOO ETF will cost you {round(most_recent_open * currency.usd_price,2)}"
-                f"Your IBI buying commission is 7.5$ so the commission will be {round(7.5 * currency.usd_price, 2)}\n\n"
+                f"ALERT! VOO opened at {most_recent_open}$, which is more than 15% below the average open price over the past month.\n\n"
+                f"Average open price: {average_open}$\n"
+                f"Current price: {most_recent_open}$\n\n"
+                f"the price of USD is currently {currency.usd_price}₪, and a VOO ETF will cost you {round(most_recent_open * currency.usd_price,2)}₪"
+                f"Your IBI buying commission is 7.5$ so the commission will be {round(7.5 * currency.usd_price, 2)}₪\n\n"
                 "This is a significant drop. Consider buying more VOO according to your investment strategy.\n"
             )
             
@@ -42,11 +42,11 @@ def lambda_handler(event, context):
         elif most_recent_open < average_open * 0.95:
             subject = f'VOO Alert: VOO opened at {most_recent_open} - Buy Opportunity!'
             message = (
-                f"VOO opened at {most_recent_open}, which is slightly lower than the average open price over the past month.\n\n"
-                f"Average open price: {average_open}\n"
-                f"Current price: {most_recent_open}\n"
-                f"the price of USD is currently {currency.usd_price}, and a VOO ETF will cost you {round(most_recent_open * currency.usd_price,2)}\n"
-                f"Your IBI buying commission is 7.5$ so the commission will be {round(7.5 * currency.usd_price, 2)}\n\n"
+                f"VOO opened at {most_recent_open}$, which is slightly lower than the average open price over the past month.\n\n"
+                f"Average open price: {average_open}$\n"
+                f"Current price: {most_recent_open}$\n"
+                f"the price of USD is currently {currency.usd_price}₪, and a VOO ETF will cost you {round(most_recent_open * currency.usd_price,2)}₪\n"
+                f"Your IBI buying commission is 7.5$ so the commission will be {round(7.5 * currency.usd_price, 2)}₪\n\n"
                 "This might be a good time to buy more VOO. Check your investment strategy."
             )
             
@@ -64,10 +64,10 @@ def lambda_handler(event, context):
         else:
             subject = f'VOO Status: Normal Trading Level at {most_recent_open}'
             message = (
-                f"VOO is trading at {most_recent_open}, which is within the normal range.\n\n"
-                f"Average open price over the past month: {average_open}\n"
-                f"the price of USD is currently {currency.usd_price}, and a VOO ETF will cost you {round(most_recent_open * currency.usd_price,2)}\n"
-                f"Your IBI buying commission is 7.5$ so the commission will be {round(7.5 * currency.usd_price, 2)}\n\n"
+                f"VOO is trading at {most_recent_open}$, which is within the normal range.\n\n"
+                f"Average open price over the past month: {average_open}$\n"
+                f"the price of USD is currently {currency.usd_price}₪, and a VOO ETF will cost you {round(most_recent_open * currency.usd_price,2)}₪\n"
+                f"Your IBI buying commission is 7.5$ so the commission will be {round(7.5 * currency.usd_price, 2)}₪\n\n"
                 "No significant price drop detected. No action required."
             )
             
